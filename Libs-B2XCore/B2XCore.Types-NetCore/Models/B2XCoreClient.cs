34c2516fd4fc5a112a55dda64304bc6ca571e37b
montague247 <woop@Dirks-MacBook-Pro.local>
2017-05-30 02:25:47 +0200

    .

--- Libs-B2XCore/B2XCore.Types-NetCore/Models/B2XCoreClient.cs
+++ Libs-B2XCore/B2XCore.Types-NetCore/Models/B2XCoreClient.cs
@@ -1,1 +1,55 @@
-../../../../B2XCore/Libs/B2XCore.Types/Models/B2XCoreClient.cs
+using System.Collections.Generic;
+
+namespace B2XCore.Models
+{
+	/// <summary>
+	/// Entry of table B2XCoreClient.
+	/// </summary>
+	[DataContract(Namespace = Constants.CoreModelTypeNamespace)]
+	public partial class B2XCoreClient : ChangeTracking
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="B2XCoreClient"/> class.
+        /// </summary>
+        public B2XCoreClient()
+            : base(typeof(B2XCoreClientHistory))
+        {
+        }
+
+		/// <summary>
+		/// Gets or sets the ClientID.
+		/// </summary>
+		/// <value>
+		/// The ClientID.
+		/// </value>
+		[DataMember]
+		public int ClientId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the UniqueID.
+		/// </summary>
+		/// <value>
+		/// The UniqueID.
+		/// </value>
+		[DataMember]
+		public Guid UniqueKey { get; set; }
+
+		/// <summary>
+		/// Gets or sets the Name.
+		/// </summary>
+		/// <value>
+		/// The Name.
+		/// </value>
+		[DataMember]
+		public string Name { get; set; }
+
+		/// <summary>
+		/// Gets or sets the ApplicationID.
+		/// </summary>
+		/// <value>
+		/// The ApplicationID.
+		/// </value>
+		[DataMember]
+		public int ApplicationId { get; set; }
+    }
+}
--- Libs-B2XCore/B2XCore.Types-NetCore/Models/B2XCoreClientHistory.cs
+++ Libs-B2XCore/B2XCore.Types-NetCore/Models/B2XCoreClientHistory.cs
@@ -1,1 +1,41 @@
-../../../../B2XCore/Libs/B2XCore.Types/Models/B2XCoreClientHistory.cs
+namespace B2XCore.Models
+{
+	/// <summary>
+	/// Entry of table B2XCoreClientHistory.
+	/// </summary>
+	[DataContract(Namespace = Constants.CoreModelTypeNamespace)]
+	public partial class B2XCoreClientHistory : ChangeHistory
+    {
+        /// <summary>
+        /// Gets or sets the ClientID.
+        /// </summary>
+        /// <value>
+        /// The ClientID.
+        /// </value>
+        public int ClientId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the UniqueID.
+        /// </summary>
+        /// <value>
+        /// The UniqueID.
+        /// </value>
+        public Guid UniqueKey { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Name.
+        /// </summary>
+        /// <value>
+        /// The Name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the ApplicationID.
+        /// </summary>
+        /// <value>
+        /// The ApplicationID.
+        /// </value>
+        public int ApplicationId { get; set; }
+    }
+}
